aed5bab7246542a1bdc07909d322049bfe534964
MITHRABR21 <mithram228@gmail.com>
2026-08-30 00:39:24 +0530

    Fix GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
-import Cakes from "./pages/Cakes.jsx";
-import IceCreams from "./pages/IceCreams.jsx";
-import Sundaes from "./pages/Sundaes.jsx";
-import Falooda from "./pages/Falooda.jsx";
-import OrderSuccess from "./pages/OrderSuccess.jsx";
 
 import "./index.css";
 
@@ -15,37 +10,8 @@ ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<App />} />
-
-        <Route
-          path="/cakes"
-          element={<Cakes />}
-        />
-
-        <Route
-          path="/icecreams"
-          element={<IceCreams />}
-        />
-
-        <Route
-          path="/sundaes"
-          element={<Sundaes />}
-        />
-
-        <Route
-          path="/falooda"
-          element={<Falooda />}
-        />
-
-        <Route
-          path="/order-success"
-          element={<OrderSuccess />}
-        />
-
-      </Routes>
+    <BrowserRouter basename="/SweetSpot">
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
